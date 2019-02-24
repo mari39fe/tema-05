270774fdde42c4469e5a99efbd89745388e120be
Maria Scheer Norreen <mariascheernorreen@Marias-MacBook-Pro-6.local>
2019-02-24 19:41:53 +0100

    lol hvad

--- basilico/script.js
+++ basilico/script.js
@@ -26,3 +26,62 @@ ikon.addEventListener("click", function () {
     }
 
 })
+
+// HER STARTER MENUEN MED GRID OG JSON
+
+let menu = [];
+let filter = "pizza";
+
+document.addEventListener("DOMContentLoaded", start);
+
+
+function start() {
+
+    let destination = document.querySelector("#liste");
+
+    hentJson();
+
+
+
+    async function hentJson() {
+
+        let jsonData = await fetch("menu.json");
+        console.log(jsonData);
+        personer = await jsonData.json();
+        console.log(menu);
+
+    })
+derSkalVæreEnFunktion();
+
+}
+
+
+function derSkalVæreEnFunktion() {
+    destination.innerHTML = ""
+    menu.forEach(menuItem => {
+        if (filter == "pizza" || menuItem.kategori == filter) {
+            destination.innerHTML += `<div id="box"><h2>${menuItem.titel}</h2> <img src=${menuItem.billede}><p>${menuItem.beskrivelse}</p><p>${menuItem.pris}</p></div>`
+        }
+    })
+
+
+
+
+}
+
+document.querySelectorAll(".filter").forEach(elm => {
+    elm.addEventListener("click", filtrering);
+})
+
+function filtrering() {
+    filter = this.getAttribute("data-hold");
+    document.querySelector("h1").textContent = this.textContent;
+    document.querySelectorAll(".filter").forEach(elm => {
+        elm.classList.remove("valgt");
+    })
+    this.classList.add("valgt");
+
+    derSkalVæreEnFunktion();
+}
+
+}
